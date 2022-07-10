46ca110c73413343029cfaca688e0cfd50bdc989
jayzumen <reinhardtjanniklas96@gmail.com>
2022-07-10 17:14:07 +0200

    s

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="h-screen w-full bg-gradient-to-r from-blue-500 to-blue-900">
+        <div className="flex justify-center flex-wrap">
+          <div className="h-52 w-52 border-2 mx-2 my-2 rounded-md">
+            <img src="" alt="" />
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className="flex flex-row items-center justify-between bg-gradient-to-r from-blue-500 to-blue-900">
+      <div className="flex flex-row items-center justify-between bg-gradient-to-r from-blue-800 to-gray-900">
         <div className="ml-2 cursor-pointer">
           <a href={Home}>
             <IoMdRestaurant size={50} />
@@ -48,7 +48,7 @@ const Navbar = () => {
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
         {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400 p-2">
+          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-gray-900 to-blue-900 text-gray-400 p-2">
             {links.map(({ id, link }) => (
               <li
                 key={id}
